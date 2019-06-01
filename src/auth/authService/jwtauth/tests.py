cc44112e2116ef94603734d56d7ab3ad1b638d16
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-06-01 00:14:33 -0600

    Testing vim plugin fugitive

--- src/auth/authService/jwtauth/tests.py
+++ src/auth/authService/jwtauth/tests.py
@@ -49,6 +49,9 @@ class AuthGetToken(TestCase):
         payload = decode_token(token)
         self.assertEquals(user_id, payload['user_id'])
 
+    def test_test(self):
+        pass
+
 
 class AuthJWTTestCase(TestCase):
     """
